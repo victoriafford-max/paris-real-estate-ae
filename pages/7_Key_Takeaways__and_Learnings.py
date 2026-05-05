fca9b79a719d5c74284cbd8c8df5011fc99eae50
tori <victoriafford@gmail.com>
2026-05-05 16:58:33 +0200

    Fix formatting for project objectives and challenges

--- pages/7_Key_Takeaways__and_Learnings.py
+++ pages/7_Key_Takeaways__and_Learnings.py
@@ -49,11 +49,11 @@ st.header("Did we achieve our project objectives?")
 st.markdown("""
 We laid the technical foundation for monitoring property values and rent control data, achieving most of our objectives:   
 
-:white_check_mark:    A robust database schema
+* :white_check_mark:  A robust database schema
 
-:white_check_mark:    Analytical visualisations tracking key metrics and patterns
+* :white_check_mark:  Analytical visualisations tracking key metrics and patterns
 
-:toolbox:    An efficient ETL pipeline...
+* :toolbox:  An efficient ETL pipeline...
 """)
 
 # -------------------------
@@ -66,18 +66,19 @@ st.markdown("""
 
 **Manual Uploads Slow Pipeline and Increase Risk of Error**	
 
-:toolbox:    The main weakness of our ETL pipeline is the manual uploads involved at multiple steps in the pipeline. 
+* :toolbox:  The main weakness of our ETL pipeline is the manual uploads involved at multiple steps in the pipeline. 
 
-:white_check_mark:    To ensure consistency, there are checkpoints along the way were necessary data uploads are flagged.
+* :white_check_mark:  To ensure consistency, there are checkpoints along the way were necessary data uploads are flagged.
 
-:bulb:    Looking forward, one solution would be to integrate the entire pipeline into Snowflake (which was not possible with a trial account).
+* :bulb:  Looking forward, one solution would be to integrate the entire pipeline into Snowflake (which was not possible with a trial account).
+
 
 **Joining Geographic Data**
 
-:toolbox:    One significant challenge we faced is how to integrate geographic data into our schema. Since the relationships between variables are spacial, a traditional primary key / foreign key relationship isn't applicable.
+* :toolbox:  One significant challenge we faced is how to integrate geographic data into our schema. Since the relationships between variables are spacial, a traditional primary key / foreign key relationship isn't applicable.
 
-:white_check_mark:     Since the scope of our project didn't require a full geospatial schema, our hybrid star model sufficed.
+* :white_check_mark:  Since the scope of our project didn't require a full geospatial schema, our hybrid star model sufficed.
 
-:bulb:    Looking forward, 
+* :bulb:  Looking forward, if the further development of a geospatial layer is key aspect of the analysis, working with PostgreSQL and PostGIS would be a more suitable option.
 
 """)
