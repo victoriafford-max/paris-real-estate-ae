2f1dc341e8012c54491b792c85d8e539af11181e
tori <victoriafford@gmail.com>
2026-05-06 14:44:14 +0200

    Refactor key findings layout into two columns
    Reorganize key findings into two columns for better layout.

--- pages/8_Conclusion.py
+++ pages/8_Conclusion.py
@@ -37,13 +37,22 @@ st.caption(
 #  Conclusions of the analysis
 # -------------------------
 st.markdown("---") 
-
 st.header("Key findings from the data")
-st.info("""
-* **High reference rent values follow high property prices**: The high demand, short supply of small, centrally located units translates to higher prices in both the sale and rental markets.   
-* **Low market liquidity central areas**: High reference rent zones tend to have lower transaction volumes. In the case of 1-room properties, this may reflect an owner's preference for holding onto smaller, centrally located units.
-* **The most expensive living areas aren't the most green**: Location and centrality are stronger price drivers, while green spaces provide useful additional context.
-""")
+
+col1, col2 = st.columns(2)
+with col1:
+    st.info("""
+    **High reference rent values follow high property prices**: The high demand, short supply of small, centrally located units translates to higher prices in both the sale and rental markets.   
+        """)
+    st.info("""
+    **Low market liquidity central areas**: High reference rent zones tend to have lower transaction volumes. In the case of 1-room properties, this may reflect an owner's preference for holding onto smaller, centrally located units.
+    """)
+with col2:
+    st.info("""
+        **The most expensive living areas aren't the most green**: Location and centrality are stronger price drivers, while green spaces provide useful additional context.
+        """)
+
+
 
 # -------------------------
 #  Challenges and Lessons Learned
